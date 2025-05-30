f0e577b3e1d0be26e5459d58dceadcf33b1e0db7
Guddu Kumar <guddu748081@gmail.com>
2025-05-30 09:18:50 +0530

    tailwind css setup done

--- backend/routers/conversationRouter.js
+++ backend/routers/conversationRouter.js
@@ -4,12 +4,10 @@ import { deleteConversation, getConversations, newConversation,newMessage } from
 const conversationRouter = express.Router();
 
 
-
 conversationRouter.get("/conversation", getConversations);
 conversationRouter.post("/conversation", newConversation);
 conversationRouter.put("/conversation/:id", newMessage);
 conversationRouter.delete("/conversation/:id", deleteConversation);
-
 
 
 export { conversationRouter };
